5f73e63112decd0bee4c60ca771a3fb9a866ecfa
Oliver English <olivertorres1997@gmail.com>
2026-09-18 21:36:13 -0600

    eliminar componentes que no pertenecen al proyecto

--- resources/views/components/site/account/table.blade.php
+++
@@ -1,5 +0,0 @@
-<div class="overflow-x-auto w-full custom-scrollbar">
-    <table class="w-full text-left border-collapse min-w-[900px]">
-        {{ $slot }}
-    </table>
-</div>
--- resources/views/components/site/geral/title-section.blade.php
+++
@@ -1,2 +0,0 @@
-@props(['title'])
-<h3 class="font-headline-lg text-base lg:text-headline-lg text-on-surface-variant">{{ $title }}</h3>
